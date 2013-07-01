d477ce4cb639100c0a6e7b0b5e20963433cc5a48
Rob Nichols <git@nicholshayes.co.uk>
2013-07-01 13:46:48 +0100

    Adds test to check that error raised when remote partial is absent is relevant

--- test/dummy/test/unit/helpers/demos_helper_test.rb
+++ test/dummy/test/unit/helpers/demos_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
+require 'remote_partial/host_app_helper'
 
 class DemosHelperTest < ActionView::TestCase
+  include RemotePartial::HostAppHelper
+
+  def test_render_remote_partial_with_no_partial
+    assert_raise ActionView::MissingTemplate do
+      render_remote_partial 'non-existent-partial'
+    end
+  end
+
 end
