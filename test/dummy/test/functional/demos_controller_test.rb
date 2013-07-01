516781450a1ca59d3ff1ceffc0c8074ff6e9a54d
Rob Nichols <git@nicholshayes.co.uk>
2013-07-01 13:36:25 +0100

    Improves testing of fixed partial

--- test/dummy/test/functional/demos_controller_test.rb
+++ test/dummy/test/functional/demos_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class DemosControllerTest < ActionController::TestCase
-  test "should get index" do
+  def test_index
     get :index
     assert_response :success
+
   end
 
-  test "should get show" do
+  def test_show_fixed
     get :show, id: 'fixed'
     assert_response :success
+    assert_match content_of('fixed'), response.body
+  end
+
+  def content_of(remote_partial_name)
+    File.read(Rails.root + "app/views/remote_partials/_#{remote_partial_name}.html.erb")
   end
 
 end
