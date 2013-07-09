af07069fcf7366d55f6eccd814003fdfd21bc774
Rob Nichols <git@nicholshayes.co.uk>
2013-07-09 11:12:09 +0100

    Adds test for partial table to RemotePartial.define

--- lib/remote_partial.rb
+++ lib/remote_partial.rb
@@ -10,7 +10,16 @@ end
 module RemotePartial
 
   def self.define(args = {})
-    Builder.build(args)
+    if Partial.table_exists?
+      Builder.build(args)
+    else
+      unless @remote_partial_table_missing_message_logged
+        message = "RemotePartial.define will not run until the '#{Partial.table_name}' table exists. See RemotePartial README"
+        puts message
+        Rails.logger.warn message
+        @remote_partial_table_missing_message_logged = true
+      end
+    end
   end
 
   def self.partial_location
