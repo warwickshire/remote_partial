449b68b84aded844d864b20af31d1aa1abd28361
Rob Nichols <git@nicholshayes.co.uk>
2013-07-09 11:42:34 +0100

    Bump version

--- lib/remote_partial/version.rb
+++ lib/remote_partial/version.rb
@@ -1,9 +1,23 @@
 module RemotePartial
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
 
 # History
 # =======
+#
+# 0.2.1 Initial migrate issue
+# ---------------------------
+# As you need to load the application, before you can migrate, the
+# RemotePartial.define statements would be called before the partial migration
+# could be called. This would raise an error:
+#
+#   "Could not find table 'remote_partial_partials'".
+#   
+# So define has to check if a partials table exists, before running build. This
+# version adds that check
+#
+# Also improved the way Webmock was loaded, and made some mods to make engine
+# more Rails 4 friendly
 #
 # 0.2.0 UTF-8 Fix
 # ---------------
